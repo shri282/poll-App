7bc8ff2fc8993bbcfbe1d29c7adfea590f0e4edc
Harishkumar33-code <harishkumar.thirupathi@softsuave.com>
2024-08-14 20:39:24 +0530

    login, register added

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.index')
+
+<link rel="stylesheet" href="css/login.css">
+
+@section('content')
+
+<form action="" method="get" class="login-form">
+    <div class="username">
+        <label for="name">User Name:</label>
+        <input type="text" name="" id="">
+    </div>
+    <div class="email">
+        <label for="email">Email:</label>
+        <input type="email" name="email" id="">
+    </div>
+    <div class="password">
+        <label for="password">Password:</label>
+        <input type="password" name="password" id="">
+    </div>
+    <div class="confirm-password">
+        <label for="password">Confirm Password:</label>
+        <input type="password" name="password" id="">
+    </div>
+    <button type="submit">Register</button>
+</form>
+
+@endsection
